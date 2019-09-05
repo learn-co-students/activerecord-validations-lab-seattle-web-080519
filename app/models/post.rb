39c3381aa61bc4dd10fafeb2f51cd1c87cad86d5
github username <github email address>
2019-09-04 17:49:23 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :cb?
+
+    CLICKYCLIKCY = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]*/i,
+        /Guess/i
+    ]
+    def cb?
+        if CLICKYCLIKCY.none? {|clicky| clicky.match title}
+            errors.add(:title, "must be clicky clicky")
+        end
+    end
 end
